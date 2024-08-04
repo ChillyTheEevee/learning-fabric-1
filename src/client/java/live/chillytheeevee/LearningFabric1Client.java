30b4b556d84ee14c26a1436c03b3196736092d80
ChillyTheEevee <myersdan@studentmail.conroeisd.net>
2024-08-03 21:47:14 -0500

    Create LearningFabric1 Mod

---
+++ src/client/java/live/chillytheeevee/LearningFabric1Client.java
@@ -0,0 +1,10 @@
+package live.chillytheeevee;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningFabric1Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
